373491383e7377ab6dc0baf4ad4d345be2f0f617
Lucas Rodrigues <136361360+Ellukita97@users.noreply.github.com>
2024-10-26 23:51:55 -0300

    Generación de QR Code sin la conexion a la base de datos

---
+++ src/mocks/experiments-mocks.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: "Experimento 1",
+    qrImage:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/QR_Code_Example.svg/1200px-QR_Code_Example.svg.png",
+  },
+  {
+    id: 2,
+    title: "Experimento 2",
+    qrImage:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/QR_Code_Example.svg/1200px-QR_Code_Example.svg.png",
+  },
+  {
+    id: 3,
+    title: "Experimento 3",
+    qrImage:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/QR_Code_Example.svg/1200px-QR_Code_Example.svg.png",
+  },
+];
