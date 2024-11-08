55e67f1ab3df4f358ed78fa86596bd05466956d2
Nicolás Villegas <nikoyt75@gmail.com>
2024-11-08 12:10:30 -0300

    finished the upload of substratums for the experiments(?

--- src/pages/api/iterations.js
+++ src/pages/api/iterations.js
@@ -5,14 +5,13 @@ export async function POST({ request }) {
   const payload = await request.json();
 
   let { data, error } = await uploadExperimentIterationPhoto(payload);
-
   if (error) {
     return new Response(JSON.stringify(error), { status: 400 });
   }
+
   //add the substratums to the experiment if there is a need to
   const { data: d, error: e } = await linkSubstratumsToExperiment(payload);
   if (e) {
-    console.log("error: ", e);
     return new Response(JSON.stringify(e), { status: 400 });
   }
 
